9ec9229fe192d9cd015ffd8bc9b749f7a162fe78
cassylee-7 <cassandrasyllee@gmail.com>
2025-01-24 10:06:08 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -28,25 +28,21 @@ public class PigLatin {
 	    }
     }
     public int findFirstVowel(String sWord) {
-	  for (int i = 0; i < sWord.length(); i++) {
-	    if (sWord.substring(i, i + 1).equals("a") || sWord.substring(i, i + 1).equals("e") || sWord.substring(i, i + 1).equals("i") || sWord.substring(i, i + 1).equals("o") || sWord.substring(i, i + 1).equals("u")) {
-	 	return i;
-		break;
-	    }
-	      return -1;
-	    }
-	  }
+	for (int i = 0; i < sWord.length(); i++) {
+    		if (sWord.substring(i, i + 1).equals("a") || sWord.substring(i, i + 1).equals("e") || sWord.substring(i, i + 1).equals("i") || sWord.substring(i, i + 1).equals("o") || sWord.substring(i, i + 1).equals("u")) 
+      			return i;
+  	}
+  	return -1;
+    }
 
     public String pigLatin(String sWord) {
-	  if (findFirstVowel(sWord) == -1)
-	    return sWord + "ay";
-	  if (sWord.length() == 0)
-		  return sWord;
-	  if (sWord.substring(0, 2).equals("qu"))
-	    return sWord.substring(2) + sWord.substring(0, 2) + "ay";
-	  if (findFirstVowel(sWord) == 0)
-		  return sWord + "way";
-	  else
-	    return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
+  	if (findFirstVowel(sWord) == -1)
+    		return sWord + "ay";
+  	if (sWord.substring(0, 2).equals("qu"))
+    		return sWord.substring(2) + sWord.substring(0, 2) + "ay";
+  	if (findFirstVowel(sWord) == 0)
+    		return sWord + "way";
+  	else
+    		return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
 	}
 }//end PigLatin class
