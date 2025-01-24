668ec5698abde1956dfa3d6ff3d3cfd33e751db5
cassylee-7 <cassandrasyllee@gmail.com>
2025-01-24 09:57:12 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -39,16 +39,14 @@ public class PigLatin {
 
     public String pigLatin(String sWord) {
 	  if (findFirstVowel(sWord) == -1)
-	  {
 	    return sWord + "ay";
-	  }
 	  if (sWord.length() == 0)
 		  return sWord;
 	  if (sWord.substring(0, 2).equals("qu"))
 	    return sWord.substring(2) + sWord.substring(0, 2) + "ay";
+	  if (findFirstVowel(sWord) == 0)
+		  return sWord + "way";
 	  else
-	  {
 	    return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
-	  }
 	}
 }//end PigLatin class
